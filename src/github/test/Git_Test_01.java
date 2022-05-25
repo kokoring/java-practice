a528583f964ad9c69d020ed32251944d5fc1d7e1
kokoring <sndb01@naver.com>
2022-05-25 22:20:10 +0900

    Add

---
+++ src/github/test/Git_Test_01.java
@@ -0,0 +1,15 @@
+package github.test;
+
+public class Git_Test_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// GitHub Test
+		
+		int n = 0;
+		int m = 1;
+		System.out.println(n+m);
+	}
+
+}
